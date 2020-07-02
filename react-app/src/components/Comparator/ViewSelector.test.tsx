04a95c1167154bee0e95f0531ecb1b59f1152e60
Erik Pearson <eapearson@lbl.gov>
2020-07-02 11:57:08 -0700

    bring view selector test to 100% coverage

--- react-app/src/components/Comparator/ViewSelector.test.tsx
+++ react-app/src/components/Comparator/ViewSelector.test.tsx
@@ -7,6 +7,8 @@ import { View } from '.';
 
 const TIMEOUT = 10000;
 
+
+
 test('renders difference type selector', async () => {
     const props: ViewSelectorProps = {
         view: 'sample',
@@ -15,14 +17,39 @@ test('renders difference type selector', async () => {
         }
     };
 
-    const { getByTestId, getByText } = render(<ViewSelector {...props} />);
+    const { getByText } = render(<ViewSelector {...props} />);
+
+    const sampleInfoElement = getByText('Sample Info');
+    expect(sampleInfoElement).toBeInTheDocument();
+    const metadataElement = getByText('Metadata');
+    expect(metadataElement).toBeInTheDocument();
+    const userMetadataElement = getByText('User Metadata');
+    expect(userMetadataElement).toBeInTheDocument();
+
+});
+
+test('switches views', async () => {
+    let currentView: string | null;
+    const props: ViewSelectorProps = {
+        view: 'sample',
+        changeView: (view: View) => {
+            currentView = view;
+        }
+    };
+
+    const { getByText } = render(<ViewSelector {...props} />);
+
+    const sampleInfoElement = getByText('Sample Info');
+    expect(sampleInfoElement).toBeInTheDocument();
+    const metadataElement = getByText('Metadata');
+    expect(metadataElement).toBeInTheDocument();
+    const userMetadataElement = getByText('User Metadata');
+    expect(userMetadataElement).toBeInTheDocument();
+
+    userMetadataElement.click();
+
     await waitFor(() => {
-        const sampleInfoElement = getByText('Sample Info');
-        expect(sampleInfoElement).toBeInTheDocument();
-        const metadataElement = getByText('Metadata');
-        expect(metadataElement).toBeInTheDocument();
-        const userMetadataElement = getByText('User Metadata');
-        expect(userMetadataElement).toBeInTheDocument();
+        expect(currentView).toEqual('user_metadata');
     }, {
         timeout: TIMEOUT
     });
