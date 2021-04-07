22167daa66449fed1c251f7c055a44e2e6ab06dd
Erik Pearson <eapearson@lbl.gov>
2021-04-07 09:00:49 -0700

    refactor authorization -> authentication

--- react-app/src/redux/middleware/fun.ts
+++ react-app/src/redux/middleware/fun.ts
@@ -3,7 +3,6 @@ import accessFun from "./access";
 import linkedDataFun from "./linkedData";
 import sampleFun from "./sample";
 import { AsyncProxyFactory } from "@kbase/ui-components/lib/redux/middleware/AsyncProxy"
-import developFun from "./develop/develop";
 
 export function makeFan() {
   const asyncProxyFactory = new AsyncProxyFactory<StoreState>();
@@ -17,6 +16,5 @@ export function makeFan() {
   asyncProxyFactory.add(sampleFun);
   asyncProxyFactory.add(linkedDataFun);
   asyncProxyFactory.add(accessFun);
-  // asyncProxyFactory.add(developFun);
   return asyncProxyFactory.createMiddleware();
 }
